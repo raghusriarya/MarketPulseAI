ac8f5a0c149117dfed94e715f144a9932b4bea1c
RBE3494501 <RBE3494501@ysil.in>
2026-04-28 14:42:18 +0530

    clean commit without secrets

--- ai-backend/server.js
+++ ai-backend/server.js
@@ -10,7 +10,12 @@ import fetch from "node-fetch";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.json());
 
 // ===============================
